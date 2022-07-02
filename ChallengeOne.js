b8d08ee38686f0226b704cc66d65f7e9cb3bfc79
Divy <divyparekh1810@gmail.com>
2022-07-02 22:12:17 +0530

    Done with challengeOne

---
+++ ChallengeOne.js
@@ -0,0 +1,23 @@
+console.log(10 + "20");
+console.log(9 - "5");
+console.log("Java" + "Script");
+console.log(" " + " ");
+console.log(" " + 0);
+console.log("Divy" - "Parekh");
+console.log(true + true);
+console.log(true + false);
+console.log(false + true);
+console.log(false - true);
+
+
+
+1020
+4
+JavaScript
+
+ 0  
+NaN
+2
+1
+1
+-1
